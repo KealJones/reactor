646fc540fd6f9b51b7a732f4fbd3cf1a2dc8fc74
Keal Jones <kealjones@gmail.com>
2019-08-26 11:58:14 -0700

    nicer script tag insertion for tests

--- lib/src/testing/utils.dart
+++ lib/src/testing/utils.dart
@@ -5,15 +5,11 @@ import 'package:reactor/reactor.dart';
 const contentDivId = 'content';
 
 enableTestMode() {
-  var reactJsScript = document.createElement('script');
-  var reactDomJsScript = document.createElement('script');
+  var reactJsScript = ScriptElement();
+  var reactDomJsScript = ScriptElement();
   var contentDiv = (new DivElement()..id = contentDivId);
-  reactJsScript.attributes.addAll({
-    'src': 'packages/reactor/js/react.development.js',
-  });
-  reactDomJsScript.attributes.addAll({
-    'src': 'packages/reactor/js/react-dom.development.js',
-  });
+  reactJsScript.src = 'packages/reactor/js/react.development.js';
+  reactDomJsScript.src = 'packages/reactor/js/react-dom.development.js';
   document.body.append(reactJsScript);
   document.body.append(reactDomJsScript);
   document.body.append(contentDiv);
