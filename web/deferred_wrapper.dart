e494996360a616460b6cf3c31549944a3477484d
Keal Jones <kealjones@gmail.com>
2019-10-11 22:13:50 -0700

    remove print & darfmt

--- web/deferred_wrapper.dart
+++ web/deferred_wrapper.dart
@@ -11,20 +11,29 @@ class DeferredWrapperStateInterface {
   bool showDeferred;
   bool deferredLoaded;
 }
-class DeferredWrapperState extends State implements DeferredWrapperStateInterface{}
+
+class DeferredWrapperState extends State implements DeferredWrapperStateInterface {}
+
 @ReactorComponent()
 DeferredWrapperComponent(Props props) {
-  var state = useState(DeferredWrapperState()..showDeferred = false..deferredLoaded = false);
+  var state = useState(DeferredWrapperState()
+    ..showDeferred = false
+    ..deferredLoaded = false);
   if (state.value.showDeferred == true && (state.value.deferredLoaded == false)) {
-    deferred_component.loadLibrary().then((v){
-      print(v);
-      print('settingState');
-      state.set(DeferredWrapperState()..showDeferred = true..deferredLoaded = true);
+    deferred_component.loadLibrary().then((v) {
+      state.set(DeferredWrapperState()
+        ..showDeferred = true
+        ..deferredLoaded = true);
     });
   }
   if (state.value.deferredLoaded == true) {
     return deferred_component.DeferredC()();
   }
-  
-  return (Dom.button()..onClick = (e) { state.set(DeferredWrapperState()..deferredLoaded = false..showDeferred = true); })('Click Me to load deferred Component');
+
+  return (Dom.button()
+    ..onClick = (e) {
+      state.set(DeferredWrapperState()
+        ..deferredLoaded = false
+        ..showDeferred = true);
+    })('Click Me to load deferred Component');
 }
